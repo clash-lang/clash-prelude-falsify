23425823a6f727e4da05dd8a4d0f55da7b9b008c
Christiaan Baaij <christiaan.baaij@gmail.com>
2026-06-11 16:09:37 +0200

    Add minimal testsuite

--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,23 @@
 module Main (main) where
 
+import Test.Tasty
+import Test.Tasty.Falsify
+
+import Test.Falsify
+import qualified Test.Falsify.Generator as Gen
+import qualified Test.Falsify.Predicate as P
+
 import MyLib ()
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain $ testGroup "clash-prelude-falsify" [
+      testGroup "Sanity" [
+          testProperty "minimum" (prop_bool_minimum False)
+        ]
+    ]
+
+prop_bool_minimum :: Bool -> Property ()
+prop_bool_minimum target =
+    testMinimum (P.expect target) $ do
+      b <- gen $ Gen.bool target
+      testFailed b
